971bcd307c0fdd9c703bddaaaae31843929f2d20
Chris <chrisanguyen@gmail.com>
2018-12-14 15:39:24 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// Button Click Listener
+var drumList = document.querySelectorAll(".drum");
+for (var i = 0; i < drumList.length; i++) {
+  drumList[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    playSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+// Keyboard Listener
+document.addEventListener("keypress", function(event) {
+  playSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function playSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio('sounds/kick.mp3');
+      kick.play();
+      break;
+    default:
+  }
+}
+
+function buttonAnimation(key) {
+    var activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    }, 100);
+}
